5367bbe2a5dbef3ad81d22610b5a15e0002eec5c
Daniel Ruiz <daniel.ruiz@rapidfire.com>
2023-02-14 14:14:22 -0500

    Start project

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <p>Holas</p>
+    </div>
+  );
+};
+
+export default Counter;
